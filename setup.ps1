a0c5a115c253e7875bd9449bacb2b20a30fb21bc
DeltaXIS <51770933+DeltaXIS@users.noreply.github.com>
2021-06-22 13:56:46 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,12 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install python -y
+choco install git.install -y
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Update-SessionEnvironment
 
 python -m pip install -U pip
-
-choco install git.install -params '"/GitOnlyOnPath"' -y
 
 pip install azure-cognitiveservices-vision-computervision
 
